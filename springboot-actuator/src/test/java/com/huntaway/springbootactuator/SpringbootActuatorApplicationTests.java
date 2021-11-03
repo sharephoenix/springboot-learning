5e925413c651f60a979e72ab335754bebc2e1410
alexluan <326083325@qq.com>
2021-11-03 13:32:33 +0800

    Actuator 信息监控

---
+++ springboot-actuator/src/test/java/com/huntaway/springbootactuator/SpringbootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huntaway.springbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
