b238d79238f2e397df624fe15b2caeb6ab34a44a
alexluan <326083325@qq.com>
2021-11-28 09:35:19 +0800

    spring-boot version change

--- springcloud/springcloud-provider-dept-9001/src/main/java/cn/huntaway/DeptProvider_9001.java
+++ springcloud/springcloud-provider-dept-9001/src/main/java/cn/huntaway/DeptProvider_9001.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class})
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+//@EnableEu
 public class DeptProvider_9001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_9001.class, args);
